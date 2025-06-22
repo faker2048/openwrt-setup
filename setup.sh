41fbff5abc20549406c1b15a28a236e31d99fa12
fiko <nspyia2002@gmail.com>
2025-06-22 09:29:03 +0800

    Refactor setup.sh to create a dedicated setup directory and download scripts into it, improving organization and execution of setup steps.

--- setup.sh
+++ setup.sh
@@ -1,17 +1,31 @@
 #!/bin/sh
 
 BASE_URL="https://gh-proxy.com/https://raw.githubusercontent.com/faker2048/openwrt-setup/master/scripts"
+SETUP_DIR="setup/openwrt"
 
 echo "Starting OpenWrt setup..."
 
+# Create setup directory
+echo "Creating setup directory: $SETUP_DIR"
+mkdir -p "$SETUP_DIR"
+
+# Download scripts
+echo "Downloading scripts..."
+curl -fsSL "$BASE_URL/01-expend_root.sh" -o "$SETUP_DIR/01-expend_root.sh"
+curl -fsSL "$BASE_URL/02-set_up_lan_ip.sh" -o "$SETUP_DIR/02-set_up_lan_ip.sh"
+curl -fsSL "$BASE_URL/03-install-packages.sh" -o "$SETUP_DIR/03-install-packages.sh"
+
+# Make scripts executable
+chmod +x "$SETUP_DIR"/*.sh
+
 # Execute scripts in order
 echo "1. Expanding root filesystem..."
-curl -fsSL "$BASE_URL/01-expend_root.sh" | sh
+sh "$SETUP_DIR/01-expend_root.sh"
 
 echo "2. Setting up LAN IP..."
-curl -fsSL "$BASE_URL/02-set_up_lan_ip.sh" | sh
+sh "$SETUP_DIR/02-set_up_lan_ip.sh"
 
 echo "3. Installing packages..."
-curl -fsSL "$BASE_URL/03-install-packages.sh" | sh
+sh "$SETUP_DIR/03-install-packages.sh"
 
 echo "OpenWrt setup completed!"
