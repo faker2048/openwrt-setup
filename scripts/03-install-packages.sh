8bcd83b1851d58310ac4cc4972e6ee8e41f800b7
fiko <nspyia2002@gmail.com>
2025-06-22 09:23:12 +0800

    Fix gh-proxy URL in 03-install-packages.sh for homeproxy package download

--- scripts/03-install-packages.sh
+++ scripts/03-install-packages.sh
@@ -9,7 +9,7 @@ opkg install bash curl tmux
 opkg install sing-box
 
 # homeproxy modified version
-wget https://gh-proxy/https://github.com/faker2048/homeproxy/releases/download/v2025.1.20/luci-app-homeproxy_dev-1737315154-ae25be1_all.ipk
+wget https://gh-proxy.com/https://github.com/faker2048/homeproxy/releases/download/v2025.1.20/luci-app-homeproxy_dev-1737315154-ae25be1_all.ipk
 opkg install luci-app-homeproxy_dev-1737315154-ae25be1_all.ipk
 rm luci-app-homeproxy_dev-1737315154-ae25be1_all.ipk
 
